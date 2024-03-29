b4565b98e5f39203dee94cd9dc3ff8ba0feca22c
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-29 14:40:24 +0100

    light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ function App() {
     const [component] = useAtomState(componentState)
 
     return (
-        <div className="bg-zinc-950 h-screen w-screen select-none flex text-white bg">
-            <div className="w-[800px] h-[500px] m-auto p-16 bg-zinc-950 rounded-md">
-                {component}
+        <div className={window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"}>
+            <div className="bg-slate-200 dark:bg-zinc-950 h-screen w-screen select-none flex dark:text-white bg">
+                <div className="w-[800px] h-[500px] m-auto p-16 bg-zinc-200 dark:bg-zinc-950 rounded-md">
+                    {component}
+                </div>
             </div>
         </div>
     )
