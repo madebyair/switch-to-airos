cc991098c19c4aa554a69b2a0681dc9d8b727605
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-29 15:49:46 +0100

    Continue button in Welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,16 @@
 import { useTranslation } from "react-i18next"
+import Button from "./elements/Button.tsx"
 
 const Welcome = () => {
     const [ t ] = useTranslation()
 
     return (
-        <div className="relative">
+        <div className="relative h-full">
             <h1 className="text-6xl font-bold">{t("Welcome")}</h1>
-            <h2 className="w-96">{t("Welcome to Airos! Say goodbye to limits, ugly interfaces and bugs. Experience a seamless transition to a world of limitless possibilities with Airos.")}</h2>
+            <h2 className="w-96 mt-8">{t("Welcome to Airos! Say goodbye to limits, ugly interfaces and bugs. Experience a seamless transition to a world of limitless possibilities with Airos.")}</h2>
+            <div className="absolute bottom-0">
+                <Button submit={() => {}} label={t("Continue")} />
+            </div>
         </div>
     )
 }
