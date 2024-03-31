b4084339ad78c8f920bf213be3531f440021cc65
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-31 20:12:31 +0200

    "delete" installation option

--- src/components/InstallMethods.tsx
+++ src/components/InstallMethods.tsx
@@ -4,6 +4,7 @@ import { componentState } from "../state/componentState.tsx"
 import FilesToMove from "./FilesToMove.tsx"
 import { invoke } from "@tauri-apps/api/core"
 import { optionState } from "../state/appState.ts"
+import Summary from "./Summary.tsx"
 
 const InstallMethods = () => {
     const [ t ] = useTranslation()
@@ -31,6 +32,10 @@ const InstallMethods = () => {
                     </div>
                 </div>
                 <div
+                    onClick={() => {
+                        setComponent(<Summary />)
+                        setOption("delete")
+                    }}
                     className="w-full h-44 mt-8 bg-slate-300 hover:bg-slate-300/50 dark:bg-zinc-900 dark:hover:bg-zinc-900/70 transition duration-300 rounded-md">
                     <div className="h-1/2 ml-4 flex">
                         <h1 className="font-medium text-lg mt-auto mb-auto">{t("Erase ALL data and install Airos")}</h1>
